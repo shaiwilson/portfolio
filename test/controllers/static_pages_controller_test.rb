34adb4518c5878de4e773007fcc3b2b664836b87
Shai Wilson <sjwilson2@usfca.edu>
2015-07-12 14:33:01 +0900

    finish all static pages, add blog

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Shai Wilson"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
